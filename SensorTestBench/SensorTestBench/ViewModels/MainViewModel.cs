75933ead6dbe489bf1f52f2ff66c943c24d449c9
Kevin Gibril Kamdem Tcheunche <kevin.kamdem@hotmail.com>
2026-09-17 20:49:44 +0200

    Add initial MainViewModel

---
+++ SensorTestBench/SensorTestBench/ViewModels/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using SensorTestBench.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SensorTestBench.ViewModels
+{
+    internal class MainViewModel
+    {
+        // variable de champs
+        private string sensorTyp = "";
+        private double temperaturRef;
+        private double measuredResistanceValue;
+        private TestResult? lastTestResult;
+        private double tolerance;
+
+
+        // les proprietés
+        public string SensorTyp
+        {
+            get { return sensorTyp; }
+            set { sensorTyp = value; }
+        }
+        public double TemperaturRef
+        {
+            get { return temperaturRef; }
+            set {  temperaturRef = value; }
+        }
+        public double MeasuredResistance
+        {
+            get { return measuredResistanceValue; }
+            set { measuredResistanceValue = value; }
+        }
+        public TestResult? LastTestResult
+        {
+            get { return lastTestResult; }
+            set { lastTestResult = value; }
+        }
+        public double Tolerance
+        {
+            get { return tolerance; }
+            set { tolerance = value; }
+        }
+
+
+    }
+}
