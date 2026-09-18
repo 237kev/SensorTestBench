a9b8d9cf32ba4e6e5db51633ffd219838bdb5343
Kevin Gibril Kamdem Tcheunche <kevin.kamdem@hotmail.com>
2026-09-18 20:21:11 +0200

    Add MainWindow DataContext and layout structure

--- SensorTestBench/SensorTestBench/MainWindow.xaml.cs
+++ SensorTestBench/SensorTestBench/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using SensorTestBench.ViewModels;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,8 @@ namespace SensorTestBench
         public MainWindow()
         {
             InitializeComponent();
+            MainViewModel mainViewModel = new MainViewModel();
+            this.DataContext = mainViewModel;
         }
     }
 }
