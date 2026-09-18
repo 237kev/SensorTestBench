65fbb612cc192363546b947ebabe212fae6e6575
Kevin Gibril Kamdem Tcheunche <kevin.kamdem@hotmail.com>
2026-09-18 17:21:48 +0200

    Add property change notifications to MainViewModel

--- SensorTestBench/SensorTestBench/ViewModels/MainViewModel.cs
+++ SensorTestBench/SensorTestBench/ViewModels/MainViewModel.cs
@@ -4,15 +4,16 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+using System.ComponentModel; // ici je veux utiliser l'interface INotifyPropertyChanged pour notifier la View de tout changement de valeur dans le Viewmodel
+
 namespace SensorTestBench.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged // MainViewModel herite/(implemente) de la classe (Interface) INotifyPropertyChanged.
     {
         // variable de champs
         private string sensorTyp = "";
         private double temperaturRef;
         private double measuredResistanceValue;
-
         private TestResult? lastTestResult;
         private double tolerance;
 
@@ -21,33 +22,67 @@ namespace SensorTestBench.ViewModels
         public string SensorTyp
         {
             get { return sensorTyp; }
-            set { sensorTyp = value; }
+            set { 
+                    if (sensorTyp != value)
+                    {
+                        sensorTyp = value;
+                        OnPropertyChanged(nameof(SensorTyp)); // Notifie la View que la propriété SensorTyp a changé
+                    }
+                    
+                }
+
         }
         public double TemperaturRef
         {
             get { return temperaturRef; }
-            set {  temperaturRef = value; }
+            set {  
+                    if(temperaturRef != value)
+                    {
+                        temperaturRef = value;
+                        OnPropertyChanged(nameof(TemperaturRef)); // Notifie la View que la propriété TemperaturRef a changé
+                    }
+
+                }
         }
         public double MeasuredResistanceValue
         {
             get { return measuredResistanceValue; }
-            set { measuredResistanceValue = value; }
+            set { 
+                    if(measuredResistanceValue != value)
+                    {
+                        measuredResistanceValue = value;
+                        OnPropertyChanged(nameof(MeasuredResistanceValue)); // Notifie la View que la propriété MeasuredResistanceValue a changé
+                    }
+                        
+                }
         }
 
-        public TestResult? LastTestResult
-        {
-            get { return lastTestResult; }
-            set { lastTestResult = value; }
-        }
+
         public double Tolerance
         {
             get { return tolerance; }
-            set { tolerance = value; }
+            set { 
+                    if(tolerance != value)
+                    {
+                        tolerance = value;
+                        OnPropertyChanged(nameof(Tolerance)); // Notifie la View que la propriété Tolerance a changé
+                    }
+                }
         }
-
+                public TestResult? LastTestResult
+        {
+            get { return lastTestResult; }
+            set { 
+                    if(lastTestResult != value)
+                    {
+                        lastTestResult = value;
+                        OnPropertyChanged(nameof(LastTestResult)); // Notifie la View que la propriété LastTestResult a changé  
+                    }
+                }
+        }
         public void Run()
         {
-            
+
             TestServices testService = new TestServices(); // je veux utiliser les methodes de la classe TestServices pour les tests de calcul
             TestResult testResult = new TestResult(); // je veux stocker les valeurs issues des tests de calcul dans notre objet metier (object de la classe testResult)
 
@@ -61,12 +96,23 @@ namespace SensorTestBench.ViewModels
             testResult.GetestetAm = DateTime.Now;
             testResult.Tolerance = Tolerance;
 
-            
+
             testResult.ResSoll = testService.ComputeResistance(TemperaturRef); // calcule de la valeur theorique de la resistance dans le TestService et stockage dans une variable l'objet result
             testResult.DeltaR = Math.Abs(testResult.ResSoll - testResult.ResIst);
             testResult.TestBestanden = testService.MeasuredResistanceIsOk(testResult.ResSoll, MeasuredResistanceValue, Tolerance); // Stockage du resultat du test dans une variable de l'objet result
 
             LastTestResult = testResult; // le MainViewModel stocke l'objet de testResult (entierement)
+
+        }
+
+
+        // Event de notification de changement de valeur
+        public event PropertyChangedEventHandler? PropertyChanged;
+    
+        //je crée la methode qui va notifier la View du changement de la valeurs de la propriétéd du MainViewModel
+        void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
