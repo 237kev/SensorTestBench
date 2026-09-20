21bef571566e3a3ed0f8eda8834ba91019252438
Kevin Gibril Kamdem Tcheunche <kevin.kamdem@hotmail.com>
2026-09-20 16:25:00 +0200

    dataset for test configuration

---
+++ SensorTestBench/SensorTestBench/Models/SensorConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SensorTestBench.Models
+{
+    internal class SensorConfig
+    {
+        // variable de champs
+        private string sensorTyp = "";
+        private double temperaturRef;
+        private double tolerance;
+        private double expectedResistance;
+
+        // proprieté
+        public string SensorTyp
+        {
+            set { sensorTyp = value; }
+            get { return sensorTyp; }
+        }
+
+        public double TemperaturRef
+        {
+            set { temperaturRef = value; }
+            get { return temperaturRef; }
+        }
+
+        public double Tolerance
+        {
+            set { tolerance = value; }
+            get { return tolerance; }
+        }
+
+        public double ExpectedResistance
+        {
+            set { expectedResistance = value; }
+            get { return expectedResistance; }
+        }
+    }
+}
